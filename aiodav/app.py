9035861e1ce9eaf52bba313d89b5c185202e8808
Gerben Aaltink <gerbenaaltink@gmail.com>
2021-02-20 03:49:32 +0100

    Added typing to user class

--- aiodav/app.py
+++ aiodav/app.py
@@ -1,9 +1,13 @@
 import pathlib
+from typing import Iterable
 
 from aiohttp import web
 
+from aiodav.user import User
 
-def create_app(root="/", users=None):
+
+def create_app(root: str = "/", users: Iterable[User] = None) -> web.Application:
     app = web.Application()
     app["root"] = pathlib.Path(root)
+    app['users'] = users
     return app
