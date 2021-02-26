f762a14f460cd0599f656170b6f7f59f7014d0ea
Gerben Aaltink <gerbenaaltink@gmail.com>
2021-02-26 07:36:09 +0100

    Added missing files

--- aiodav/views/browser.py
+++ aiodav/views/browser.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import aiohttp_jinja2
 from aiohttp.web import View
 from aiohttp.web_fileresponse import FileResponse
@@ -9,12 +11,14 @@ from aiodav.utils import safe_url
 
 class BrowserView(View):
     async def resolve_user(self) -> User:
-        return User(username="admin", password="admin", root="/")
+        return User(username="admin", password="admin", root="/home")
 
     async def get(self):
 
         user = await self.resolve_user()
-        root = user.joinpath(self.request.match_info.get("tail"))
+
+        root = pathlib.Path(self.request.match_info.get("tail"))
+        print(root)
 
         if root.is_file():
             return FileResponse(root)
@@ -30,18 +34,6 @@ class BrowserView(View):
                 )
             )
 
-        location = root
-
-        print(
-            dict(
-                a=self.request.raw_path,
-                b=self.request.path,
-                c=self.request.match_info,
-                d=self.request.path_qs,
-                e=self.request.host,
-                location=location,
-            )
-        )
         context = dict(
             tail=self.request.match_info.get("tail"),
             request=self.request,
